fdc6f8401bfd45d73b00cae9852a1335349717f6
Navaneet <126859932+sainavaneet@users.noreply.github.com>
2024-01-18 15:10:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import support as sfd
 import matplotlib.gridspec as gridspec
 import matplotlib.animation as animation
+from mpl_toolkits.mplot3d import Axes3D
+
 
 
 support = sfd.support()
